366f96d50623cfc7957be693b6d33fe100600cf8
kalpanath-krishnan <knkrish1991@gmail.com>
2024-03-15 15:07:59 +0530

    filter button added

--- components/Body.js
+++ components/Body.js
@@ -1,17 +1,28 @@
 import Card from "./Card"
 import resList from "../utils/mock_data"
+import  {useState}  from "react"
+
 const Body=()=>
 {
-    
+  const [listOfRestaurants,setListOfRestaurant]=useState(resList);
   return(
     <div className="body">
-    <div className="search">search</div>
-    <div className="res-container">
-            {
-                resList.map(restaurant=> <Card  key={restaurant.info.id} resData={restaurant}/>)
-            }
-          
-    </div>
+        <div className="search">
+           <button className="search-btn" onClick={()=>{
+            const filterdList= listOfRestaurants.filter((res)=>res.info.avgRating>4.1);
+            setListOfRestaurant(filterdList);
+           }}
+           
+            >
+             top-reastaurants
+           </button>
+        </div>
+        <div className="res-container">
+                {
+                    listOfRestaurants.map(restaurant=> <Card  key={restaurant.info.id} resData={restaurant}/>)
+                }
+              
+        </div>
 </div>
   )
 }
--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,21 @@
 import { CDN_URL } from "../utils/constants";
-
+    
 const Card =(props)=>{
     const {resData}=props;
     
-    const{name,avgRating,cuisines,costForTwo,deliveryTime}=resData?.info;
+    const{name,avgRating,cuisines,costForTwo,cloudinaryImageId,sla}=resData?.info;
 
     return(
    
-     <div className="res-card" style={{backgroundColor:"transparent"}}>
+     <div className="res-card" >
          
-         <img className="res-logo" src={CDN_URL + resData.info.cloudinaryImageId }/>
+         <img className="res-logo" src={CDN_URL + cloudinaryImageId }/>
          
          <h4>{name}</h4>
          <h4>{avgRating}</h4>
          <h4>{cuisines.join(", ")}</h4>
          <h4>{costForTwo}</h4>
-         <h4>{deliveryTime}</h4>
+         <h4>{sla.deliveryTime} minutes</h4>
      </div>
    )}
 
