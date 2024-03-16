a65f09304fef7a31409e88b5de0d637b1583ce55
kalpanath-krishnan <knkrish1991@gmail.com>
2024-03-16 13:19:18 +0530

    live api added

--- components/Body.js
+++ components/Body.js
@@ -1,15 +1,24 @@
 import Card from "./Card"
-import resList from "../utils/mock_data"
-import  {useState}  from "react"
+import  {useState,useEffect}  from "react"
 
 const Body=()=>
 {
-  const [listOfRestaurants,setListOfRestaurant]=useState(resList);
+  const [listOfRestaurants,setListOfRestaurant]=useState([]);
+  useEffect(()=>{
+    fetchData();
+ },[]);
+  const fetchData= async()=>{
+    const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.670626&lng=76.55788199999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json= await data.json();
+  setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  
+ 
+  }
   return(
     <div className="body">
         <div className="search">
            <button className="search-btn" onClick={()=>{
-            const filterdList= listOfRestaurants.filter((res)=>res.info.avgRating>4.1);
+            const filterdList= listOfRestaurants.filter((res)=>res.info.avgRating>=4.2);
             setListOfRestaurant(filterdList);
            }}
            
