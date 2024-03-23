af6a98a73e4c1ce3d0c7603df5d86c357ae8d981
kalpanath-krishnan <knkrish1991@gmail.com>
2024-03-23 10:58:32 +0530

    search bar and search function added

--- components/Body.js
+++ components/Body.js
@@ -2,9 +2,13 @@ import Card from "./Card"
 import  {useState,useEffect}  from "react"
 import Shimmer from "./Shimmer"
 
+
 const Body=()=>
 {
   const [listOfRestaurants,setListOfRestaurant]=useState([]);
+  const [searchText,setSearchText]=useState("");
+  const [filteredRestaurants,setFilteredRestaurants]=useState([]);
+  
   useEffect(()=>{
     fetchData();
  },[]);
@@ -12,6 +16,7 @@ const Body=()=>
     const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.670626&lng=76.55788199999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const json= await data.json();
   setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
   if(listOfRestaurants.length===0)
   {
@@ -28,10 +33,20 @@ const Body=()=>
             >
              top-reastaurants
            </button>
+           <input type="text" value={searchText} onChange={
+            (e)=>{
+              setSearchText(e.target.value);
+            }
+           }/>
+           <button onClick={()=>{
+            console.log(searchText);
+            const filteredRestaurants = listOfRestaurants.filter((res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+            setFilteredRestaurants(filteredRestaurants);
+           }} >search</button>
         </div>
         <div className="res-container">
                 {
-                    listOfRestaurants.map
+                    filteredRestaurants.map
                     (restaurant=> <Card  key={restaurant.info.id} resData={restaurant}/>)
                 }
               
--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,30 @@
 import { LOGO_URL } from "../utils/constants";
-const Header =()=>(
-    <div className="header">
-        <div className="logo">
-            <img src={LOGO_URL} alt="sorry"></img>
-            </div>
-            <div className="listItems">
-                <ul>
-                    <li>about</li>
-                    <li>contact</li>
-                    <li>tell me</li>
-                    <li>cart</li>
-                </ul>
-            </div>
-    </div>
-)
+import { useEffect, useState } from "react";
+
+const Header =()=>{
+    const [loginState,setLoginState]= useState("login");
+    return (
+
+        <div className="header">
+            <div className="logo">
+                <img src={LOGO_URL} alt="sorry"></img>
+                </div>
+                <div className="listItems">
+                    <ul>
+                        <li>about</li>
+                        <li>contact</li>
+                        <li>tell me</li>
+                        <button className="login" onClick={
+                            ()=>{
+                                loginState ==="login"?
+                                setLoginState("logout"):
+                                setLoginState("login");
+                            }
+                        }
+                        >{loginState}</button>
+                    </ul>
+                </div>
+        </div>
+    )
+}
 export default Header;
